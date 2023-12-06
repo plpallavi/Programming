4adbf57e8d80205d100e223676401b2f7bd36140
plpallavi <pallavi121202@gmail.com>
2023-12-06 23:58:37 +0530

    Code

---
+++ Coding Sheet/AsciiValueOfacharacter.java
@@ -0,0 +1,12 @@
+public class AsciiValueOfacharacter {
+
+    static void value() {
+        char c = 'e';
+        int ascii = c;
+        System.out.println("Ascii Value of " + c + "is: " + ascii);
+    }
+
+    public static void main(String[] args) {
+        value();
+    }
+}
