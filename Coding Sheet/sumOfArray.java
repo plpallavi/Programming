7596b8e28b662230d641d163c8112acf74732414
plpallavi <pallavi121202@gmail.com>
2023-12-18 19:45:57 +0530

    Code

---
+++ Coding Sheet/sumOfArray.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class sumOfArray {
+    public static void main(String[] args) {
+        int n = 5;
+        int arr[] = { 1, 2, 3, 4, 5 };
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += arr[i];
+        }
+        System.out.println("The sum of the elements of the array is: " + sum);
+    }
+}
