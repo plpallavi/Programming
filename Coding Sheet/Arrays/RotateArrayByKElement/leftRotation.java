96f8d56070aac93a5846307140fc03ee12fd66cd
plpallavi <pallavi121202@gmail.com>
2023-12-29 20:51:07 +0530

    Code

--- Coding Sheet/Arrays/RotateArrayByKElement/leftRotation.java
+++ Coding Sheet/Arrays/RotateArrayByKElement/leftRotation.java
@@ -1,7 +1,35 @@
 package RotateArrayByKElement;
 
 public class leftRotation {
-    public static void main(String[] args) {
 
+    public static void RotatetoLeft(int[] arr, int n, int k) {
+        if (n == 0)
+            return;
+
+        k = k % n;
+        if (k > n)
+            return;
+        int[] temp = new int[k];
+        for (int i = 0; i < k; i++) {
+            temp[i] = arr[i];
+        }
+
+        for (int i = 0; i < n - k; i++) {
+            arr[i] = arr[i + k];
+        }
+        for (int i = n - k; i < n; i++) {
+            arr[i] = temp[i - n + k];
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 7;
+        int[] arr = { 1, 2, 3, 4, 5, 6, 7 };
+        int k = 2;
+        RotatetoLeft(arr, n, k);
+        System.out.println("After rotating: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
     }
 }
