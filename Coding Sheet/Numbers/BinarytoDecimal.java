eab387ccc5093066ff0cbd6301cd1fe7bf0fd1b0
plpallavi <pallavi121202@gmail.com>
2023-12-31 20:28:54 +0530

    Java

--- Coding Sheet/Numbers/BinarytoDecimal.java
+++ Coding Sheet/Numbers/BinarytoDecimal.java
@@ -2,6 +2,16 @@ package Numbers;
 
 public class BinarytoDecimal {
     public static void main(String[] args) {
-
+        String s = "1001";
+        int n = s.length();
+        int base = 1;
+        int ans = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            if (s.charAt(i) == '1') {
+                ans += base;
+            }
+            base *= 2;
+        }
+        System.out.println(ans);
     }
 }
